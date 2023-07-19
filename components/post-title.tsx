486e0c7da40245623616827605964ccfbef5e695
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-19 18:07:03 +0100

    refine type + anim

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -1,7 +1,7 @@
 export default function PostTitle({ children }) {
   return (
     <h1
-      className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left"
+      className="text-6xl md:text-7xl lg:text-8xl font-serif font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left animate-fade-left animate-once animate-ease-out animate-duration-[1500ms]"
       dangerouslySetInnerHTML={{ __html: children }}
     />
   )
