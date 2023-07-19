5838a03eb2c1534700cb6b84fee00f28733c8404
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-19 13:23:36 +0100

    style intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,21 +3,21 @@ import { CMS_NAME, CMS_URL } from '../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+      <h1 className="text-6xl md:text-8xl font-display font-bold leading-tight text-shadow-custom md:pr-8">
+        Blog.it
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+      <h4 className="font-sans text-center text-shadow-custom md:text-left text-lg mt-5 md:pl-8">
+        A statically generated blog created with{' '}
         <a
           href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="underline hover:text-success duration-300 font-display transition-colors"
         >
           Next.js
         </a>{' '}
         and{' '}
         <a
           href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
+          className="underline hover:text-success duration-300 font-display transition-colors"
         >
           {CMS_NAME}
         </a>
