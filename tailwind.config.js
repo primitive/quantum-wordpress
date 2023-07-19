f3dad95c7aa77d7f882a404fb41d72a02d9b2d9c
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-19 10:04:43 +0100

    update color / fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
+        'accent-1': '#565656',
+        'accent-2': '#1c1917',
         'accent-7': '#333',
         success: '#0070f3',
         cyan: '#79FFE1',
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+        secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+        
       },
       spacing: {
         28: '7rem',
@@ -21,6 +24,13 @@ module.exports = {
       },
       lineHeight: {
         tight: 1.2,
+      },
+      fontFamily: {
+        // 'sans': ['ui-sans-serif', 'system-ui', ...],
+        'serif': ["ivypresto-headline", 'serif'], // 700
+        // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
+        'display': ["joschmi", 'sans-serif'], // 400
+        // 'body': ['"Open Sans"', ...],
       },
       fontSize: {
         '5xl': '2.5rem',
