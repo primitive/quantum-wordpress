b5c08715ea05cb606c90a6751283ad5591edbb57
Shaun Knowles <shaun@primitivedigital.uk>
2023-07-20 20:53:59 +0100

    refine style

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -19,7 +19,7 @@ export default function HeroPost({
         )}
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div className='animate-fade-left animate-once animate-duration[800ms] rounded-xl border-solid border-2 border-primary p-4 bg-lighter shadow-xl'>
+        <div className='animate-fade-left animate-once animate-duration[800ms] rounded-xl border-solid border-2 border-primary p-4 bg-lighter shadow-md hover:shadow-2xl transition duration-300 ease-out'>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight font-serif font-bold">
             <Link
               href={`/posts/${slug}`}
@@ -27,7 +27,7 @@ export default function HeroPost({
               dangerouslySetInnerHTML={{ __html: title }}
             ></Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className="mb-4 md:mb-0 text-lg font-display text-primary">
             <Date dateString={date} />
           </div>
         </div>
