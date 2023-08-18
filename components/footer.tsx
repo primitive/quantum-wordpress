5902cc1a4e9f95c6cfdc7dd11357d60a6bf980f2
Shaun Knowles <shaun@primitivedigital.uk>
2023-08-18 15:04:56 +0100

    fefine global parts

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,33 @@
 import Container from './container'
-import { GITHUB_PATH } from '../lib/constants'
+import { GITHUB_REPO, FOOTER_BTN_TEXT, FOOTER_BTN_LINK, FOOTER_BTN2_TEXT, FOOTER_BTN2_LINK } from '../lib/constants'
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t-4 border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold font-serif tracking-tighter leading-tight text-center text-success lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Created with Next.js.
-          </h3>
-          <p className="text-2xl lg:text-3xl font-bold tracking-tighter leading-tight text-center text-success lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Created with Next.js.
-          </p>
+          <div className="lg:w-1/2">
+            <h3 className="text-4xl lg:text-5xl font-bold font-serif tracking-tighter leading-tight text-center text-success lg:text-left mb-10 lg:mb-0 lg:pr-4">
+              Created with Next.js.
+            </h3>
+            <p className="text-2xl lg:text-3xl font-bold tracking-tighter leading-tight text-center text-success lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              Created with Next.js.
+            </p>
+          </div>
+
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href={`${FOOTER_BTN_LINK}`}
+              className="mx-3 bg-amber-600 hover:bg-amber-500 hover:text-accent-2 border border-accent-2 text-white font-bold tracking-wide py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              {FOOTER_BTN_TEXT}
             </a>
             <a
-              href={`${GITHUB_PATH}`}
-              className="mx-3 font-bold text-accent-2 hover:underline"
+              href={`${FOOTER_BTN2_LINK}`}
+              className="mx-3 font-bold text-amber-800 hover:text-amber-700 hover:underline"
+              target="_blank"
             >
-              View on GitHub
+              {FOOTER_BTN2_TEXT}
             </a>
           </div>
         </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
+import { SITE_NAME } from "../lib/constants";
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-display font-bold leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
+      <Link href="/" className="mpen">
+        {SITE_NAME}
       </Link>
-      .it
     </h2>
   )
 }
