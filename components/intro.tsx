11f71202f8b298a1a3573a61de4858f5ef4c466a
Shaun Knowles <shaun@primitivedigital.uk>
2023-08-18 13:03:07 +0100

    refine

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,28 +1,33 @@
-import { CMS_NAME, CMS_URL } from '../lib/constants'
+import { CMS_NAME, CMS_URL, SITE_NAME, THEME_NAME, INTRO_TEXT, THEME_HEADER} from "../lib/constants";
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-display font-bold leading-tight text-shadow-custom md:pr-8">
-        Blog.it
+      <h1 className="text-6xl md:text-8xl font-display font-bold leading-tight text-shadow-custom md:pr-8 mpen">
+        {`${SITE_NAME}`}
       </h1>
-      <h4 className="font-sans text-center text-shadow-custom md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog created with{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-300 font-display transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and{' '}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-300 font-display transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
-      </h4>
+      <div>
+        <h3 className="text-2xl font-display font-bold leading-tight text-center text-shadow-custom md:text-left mt-5 md:pl-8">
+          {THEME_NAME} <span className="font-sans text-lg text-secondary">{THEME_HEADER}</span>
+        </h3>
+        <h4 className="font-sans text-center text-shadow-custom md:text-left text-lg mt-5 md:pl-8">
+          {INTRO_TEXT}{" "}
+          <a
+            href="https://nextjs.org/"
+            className="underline hover:text-success duration-300 font-display transition-colors"
+          >
+            Next.js
+          </a>{" "}
+          and{" "}
+          <a
+            href={CMS_URL}
+            className="underline hover:text-success duration-300 font-display transition-colors"
+          >
+            {CMS_NAME}
+          </a>
+          .
+        </h4>
+      </div>
     </section>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import { SITE_TITLE } from '../lib/constants'
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
@@ -15,7 +15,7 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <Layout preview={preview}>
       <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+        <title>{SITE_TITLE}</title>
       </Head>
       <Container>
         <Intro />
