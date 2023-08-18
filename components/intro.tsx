c456ad771f4c78c649173e7502885ac4c9c745d1
Shaun Knowles <shaun@primitivedigital.uk>
2023-08-18 20:35:48 +0100

    extract consts

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,16 @@
-import { CMS_NAME, CMS_URL, SITE_NAME, THEME_NAME, INTRO_TEXT, THEME_HEADER} from "../lib/constants";
+import { CMS_NAME, CMS_URL, SITE_NAME, SITE_TAGLINE, THEME_NAME, INTRO_TEXT, THEME_HEADER} from "../lib/constants";
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-display font-bold leading-tight text-shadow-custom md:pr-8 mpen">
-        {`${SITE_NAME}`}
-      </h1>
+      <div>
+        <h1 className="text-6xl md:text-8xl font-display font-bold leading-tight text-shadow-custom md:pr-8 mpen">
+          {SITE_NAME}
+        </h1>
+        <p className="text-center md:text-left md:text-lg md:pl-6 mt-[-1rem] md:mt-[-1.5rem]">
+          {SITE_TAGLINE}
+        </p>
+      </div>
       <div>
         <h3 className="text-2xl font-display font-bold leading-tight text-center text-shadow-custom md:text-left mt-5 md:pl-8">
           {THEME_NAME} <span className="font-sans text-lg text-secondary">{THEME_HEADER}</span>
