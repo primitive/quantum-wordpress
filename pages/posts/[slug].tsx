ebc9c69e8394b4dee459e2f16ca8b28bd59f541a
Shaun Knowles <shaun@primitivedigital.uk>
2023-08-24 11:07:27 +0100

    update title

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -12,7 +12,7 @@ import Layout from '../../components/layout'
 import PostTitle from '../../components/post-title'
 import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
-import { CMS_NAME } from '../../lib/constants'
+import { SITE_TITLE } from '../../lib/constants'
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
@@ -33,7 +33,7 @@ export default function Post({ post, posts, preview }) {
             <article>
               <Head>
                 <title>
-                  {`${post.title} | Next.js Blog Example with ${CMS_NAME}`}
+                  {`${post.title} | ${SITE_TITLE}`}
                 </title>
                 <meta
                   property="og:image"
