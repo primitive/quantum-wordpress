513064afc4d32b8e3d1b441612b27d63b0e34c86
Shaun Knowles <shaun@primitivedigital.uk>
2023-08-25 18:21:06 +0100

    clean up

--- components/seo-ga.tsx
+++ components/seo-ga.tsx
@@ -1,15 +1,11 @@
 import Script from "next/script";
 
 export default function GA() {
-
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
   if (GA_ID) {
-  console.log("GA_ID is there", GA_ID);
-
     return (
       <>
-        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CZX17CNGCF" /> */}
         <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
@@ -25,9 +21,8 @@ export default function GA() {
         </Script>
       </>
     );
-  }
-  else {
-    console.log("GA_ID false");
+  } else {
+    console.log("GA_ID not set");
     return null;
   }
 }
