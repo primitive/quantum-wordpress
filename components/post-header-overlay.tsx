d7c2fb07994e8483fe75e8e446ab432a8cd1ea68
Shaun Knowles <shaun@primitivedigital.uk>
2023-09-04 17:37:57 +0100

    add custom

--- components/post-header-overlay.tsx
+++ components/post-header-overlay.tsx
@@ -10,11 +10,11 @@ export default function PostHeader({
 
       <div className="relative">
 
-        <div className="mb-8 md:mb-6 sm:mx-0 shadow-small border-solid border-4 border-t-8 border-primary rounded-tr-[6rem] overflow-hidden animate-fade animate-once animate-ease-out animate-delay-[1200ms] animate-duration-[1500ms]">
+        <div className="mb-8 md:mb-6 sm:mx-0 shadow-small border-solid border-4 border-t-8 border-primary rounded-tr-[6rem] overflow-hidden animate-fade animate-once animate-ease-in animate-delay-[300ms] animate-duration-[1100ms]">
           <CoverImage title={title} coverImage={coverImage} />
         </div>
 
-        <div className="absolute bottom-0 left-0 z-10 px-5 pt-3 bg-white bg-opacity-80 border-t-4 border-secondary ml-1 mr-1 mb-1">
+        <div className="absolute bottom-0 left-0 z-10 px-5 pt-3 bg-white bg-opacity-80 border-t-4 border-secondary ml-1 mr-1 mb-1 animate-fade animate-once animate-ease-in animate-delay-[1400ms] animate-duration-[1800ms]">
           <PostTitle>{title}</PostTitle>
         </div>
 
