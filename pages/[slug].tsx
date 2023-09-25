f9bce2c77e4a6f11af5beeebd1ca6832e9bd623b
Shaun Knowles <shaun@primitivedigital.uk>
2023-09-25 10:08:48 +0100

    filter out posts page from static paths

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,36 +1,35 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Head from 'next/head'
-import { GetStaticPaths, GetStaticProps } from 'next'
-import Container from '../components/container'
-import Header from '../components/header'
-import PostBody from '../components/post-body'
-import PostHeader from '../components/post-header-overlay'
-import MoreStories from '../components/more-stories'
-import PostMeta from '../components/post-meta'
-import SectionSeparator from '../components/section-separator'
-import Layout from '../components/layout'
-import GA from '../components/seo-ga'
-import PostTitle from '../components/post-title'
-import Tags from '../components/tags'
-import ContactForm from '../components/form-contact'
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Head from "next/head";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Container from "../components/container";
+import Header from "../components/header";
+import PostBody from "../components/post-body";
+import PostHeader from "../components/post-header-overlay";
+import MoreStories from "../components/more-stories";
+import PostMeta from "../components/post-meta";
+import SectionSeparator from "../components/section-separator";
+import Layout from "../components/layout";
+import GA from "../components/seo-ga";
+import PostTitle from "../components/post-title";
+import Tags from "../components/tags";
+import ContactForm from "../components/form-contact";
 // import { getPageByURI, getAllPostsWithSlug, getPageAndMorePosts } from '../lib/api'
-import { getAllPagesWithSlugs, getPageBySlug  } from '../lib/api'
-import { SITE_TITLE } from '../lib/constants'
-
+import { getAllPagesWithSlugs, getPageBySlug } from "../lib/api";
+import { SITE_TITLE } from "../lib/constants";
 
 export default function Page(page) {
-// export default function Page({ page, posts, preview }) {
-const router = useRouter()
-// const morePosts = posts?.edges
+  // export default function Page({ page, posts, preview }) {
+  const router = useRouter();
+  // const morePosts = posts?.edges
 
   if (!router.isFallback && !page?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
 
-const preview = false;
+  const preview = false;
 
-console.log('page', page)
+  console.log("page", page);
 
   return (
     <Layout preview={preview}>
@@ -42,19 +41,14 @@ console.log('page', page)
           <>
             <article>
               <Head>
-                <title>
-                  {`${page.title} / ${SITE_TITLE}`}
-                </title>
+                <title>{`${page.title} / ${SITE_TITLE}`}</title>
                 <meta
                   property="og:image"
                   content={page.featuredImage?.node.sourceUrl}
                 />
               </Head>
               <GA />
-              <PostHeader
-                title={page.title}
-                coverImage={page.featuredImage}
-              />
+              <PostHeader title={page.title} coverImage={page.featuredImage} />
               <PostBody content={page.content} />
 
               <ContactForm />
@@ -75,7 +69,7 @@ console.log('page', page)
         )}
       </Container>
     </Layout>
-  )
+  );
 }
 
 // ADV. > needs refactoring
@@ -115,7 +109,7 @@ console.log('page', page)
 
 //               <ContactForm />
 
-//               <PostMeta 
+//               <PostMeta
 //                   date={page.date}
 //                   author={page.author}
 //                   categories={page.categories}
@@ -133,7 +127,6 @@ console.log('page', page)
 //     </Layout>
 //   )
 // }
-
 
 // BASICS > working
 export async function getStaticProps({ params }) {
@@ -162,8 +155,20 @@ export async function getStaticProps({ params }) {
 // BASICS > working
 export async function getStaticPaths() {
   const pagesWithSlugs = await getAllPagesWithSlugs();
+
+  // console.log(pagesWithSlugs.edges);
+
+  // sk-dev: workaround filter out the blog posts page
+
+  const pagesWithSlugs2 = pagesWithSlugs.edges.filter(function (obj) {
+    return obj.node.slug !== "blog";
+  });
+
+  // console.log("2", pagesWithSlugs2);
+
   return {
-    paths: pagesWithSlugs.edges.map(({ node }) => `/${node.slug}`) || [],
+    // paths: pagesWithSlugs.edges.map(({ node }) => `/${node.slug}`) || [],
+    paths: pagesWithSlugs2.map(({ node }) => `/${node.slug}`) || [],
     fallback: true,
   };
 }
@@ -178,5 +183,3 @@ export async function getStaticPaths() {
 //     fallback: true,
 //   }
 // }
-
-
