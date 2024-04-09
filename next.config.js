87c485b80e7e012232035ab96ed1a1c819b354ae
Shaun Knowles <shaun@primitivedigital.uk>
2024-04-09 17:31:24 +0100

    test cors

--- next.config.js
+++ next.config.js
@@ -18,6 +18,20 @@ module.exports = {
       'secure.gravatar.com',
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://primitive.industries/" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
     // Use the CDN in production and localhost for development.
     // assetPrefix: isProd ? 'https://quantum-wordpress.vercel.app/' : undefined,
 }
