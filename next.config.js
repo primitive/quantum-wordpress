890355221ddf1237b250c3092551b4b532b17a82
Shaun Knowles <shaun@primitivedigital.uk>
2025-02-09 12:31:30 +0000

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `)
+  throw new Error("WORDPRESS_API_URL is not set. Please add it to your environment variables.");
 }
+
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
+  throw new Error("WORDPRESS_API_URL is not a valid URL. Please provide a properly formatted URL.");
+}
+
+
+const { protocol, hostname, port, pathname } = new URL(
+  process.env.WORDPRESS_API_URL,
+);
+
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: protocol.slice(0, -1),
+//         hostname,
+//         port,
+//         pathname: `${pathname}/**`,
+//       },
+//     ],
+//   },
+// };
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -25,7 +45,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://primitive.industries/" },
+          { key: "Access-Control-Allow-Origin", value: process.env.WORDPRESS_API_URL },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
